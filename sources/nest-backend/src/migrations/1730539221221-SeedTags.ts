9333f4cf012b7ef69b8c3aa6354066132cba103b
Azzurriii <vtthanh04.qb@gmail.com>
2024-11-02 17:21:53 +0700

    chore: Update transaction tags

--- sources/nest-backend/src/migrations/1730539221221-SeedTags.ts
+++ sources/nest-backend/src/migrations/1730539221221-SeedTags.ts
@@ -5,15 +5,33 @@ export class SeedTags1730539221221 implements MigrationInterface {
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
             INSERT INTO tag (name) VALUES 
-            ('Lương'),
-            ('Quà tặng'),
-            ('Chi phí ăn uống');
+            ('Salary'),           -- Lương
+            ('Gift'),             -- Quà tặng
+            ('Food Expenses'),    -- Chi phí ăn uống
+            ('Rent'),             -- Thuê nhà
+            ('Utilities'),        -- Tiện ích
+            ('Groceries'),        -- Mua sắm
+            ('Entertainment'),     -- Giải trí
+            ('Transportation'),    -- Vận chuyển
+            ('Healthcare'),       -- Chăm sóc sức khỏe
+            ('Miscellaneous');      -- Chi phí khác
         `);
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`
-            DELETE FROM tag WHERE name IN ('Lương', 'Quà tặng', 'Chi phí ăn uống');
+            DELETE FROM tag WHERE name IN (
+                'Salary', 
+                'Gift', 
+                'Food Expenses',
+                'Rent',
+                'Utilities',
+                'Groceries',
+                'Entertainment',
+                'Transportation',
+                'Healthcare',
+                'Miscellaneous'
+            );
         `);
     }
 
